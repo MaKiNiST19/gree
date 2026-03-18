8808a2386e25a40d2f1019e02188beda9dc7d3e2
MaKiNiST19 <serdar.tpbs@gmail.com>
2026-03-18 22:47:58 +0300

    google kodları eklendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import LocalBusinessSchema from '@/components/LocalBusinessSchema';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,6 +15,9 @@ export const metadata: Metadata = {
     default: 'Gree Klima İzmir Yetkili Bayi & Servis | Deytes İklimlendirme',
   },
   description: 'İzmir Gree klima yetkili bayi, profesyonel montaj ve teknik servis. Gree Inverter klima fiyatları ve ücretsiz keşif için ulaşın. Karabağlar, Konak, Buca.',
+  verification: {
+    google: 'dQZlmHTPd83Wqndf4okYZ6gB_-ONuXVlC_rZX1RZsuM',
+  },
   robots: {
     index: true,
     follow: true,
@@ -27,7 +31,50 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
+      <head>
+        {/* Google Tag Manager - Top of Head */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MT2BDHLM');
+            `,
+          }}
+        />
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-V86Q2T28NQ"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-V86Q2T28NQ');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} min-h-screen bg-gray-50 text-gray-800 flex flex-col`}>
+        {/* Google Tag Manager (noscript) - Right after body open */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MT2BDHLM"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         <Header />
         <main className="flex-1">
           {children}
